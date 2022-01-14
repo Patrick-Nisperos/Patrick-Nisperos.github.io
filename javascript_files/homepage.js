55900a90fac1c4279f3694a32d811671e267dbce
Patrick <p_nisperos@u.pacific.edu>
2022-01-13 18:44:05 -0800

    Created Working Automatic/Button pressing slideshow in the homepage, Also made the other HTML pages functional.

---
+++ javascript_files/homepage.js
@@ -0,0 +1,42 @@
+/* Javascript for the homepage */
+
+/* Code for sliding through images automatically by timer */
+var slideIndex = 0;
+showSlidesAuto();
+function showSlidesAuto() {
+  let i;
+  let slides = document.getElementsByClassName("homepage_slideshow");
+  for(i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if(slideIndex > slides.length) {
+    slideIndex = 1
+  }
+  slides[slideIndex - 1].style.display = "block";
+  setTimeout(showSlidesAuto, 7000); // Change image every 7000 milisecs(7 secs)
+}
+
+/* Code for clicking through images by arrow button */
+var slideIndex = 1;
+showSlides(slideIndex);
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("homepage_slideshow");
+  if(n > slides.length) {
+    slideIndex = 1
+  }
+  if(n < 1) {
+    slideIndex = slides.length
+  }
+  for(i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slides[slideIndex - 1].style.display = "block";
+}
